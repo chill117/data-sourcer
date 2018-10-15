3cfe1bf027c65b3825e87a2f17a8ce525e636859
Charles Hill <chill@degreesofzero.com>
2018-10-15 10:44:19 +0200

    Use requestOptions instead of urls with xml abstract

--- abstracts/xml.js
+++ abstracts/xml.js
@@ -44,31 +44,29 @@ module.exports = {
 			var onEnd = _.bind(emitter.emit, emitter, 'end');
 
 			async.each(this.config.feeds, function(feed, nextFeed) {
-				async.each(feed.urls, function(feedUrl, nextUrl) {
-					async.seq(
-						function(next) {
-							options.request({
-								url: feedUrl,
-								headers: {
-									'User-Agent': 'Mozilla/5.0 Chromium/70.0.3000.60',
-								},
-							}, function(error, response) {
-								if (error) return next(error);
-								next(null, response);
-							});
-						},
-						function(response, next) {
-							if (response.statusCode >= 400) {
-								return next(new Error(response.statusMessage));
-							}
-							parseXml(response.body, next);
-						},
-						function(xml, next) {
-							try {
-								var items = getValueAtPath(xml, feed.itemsPath)
-								items = _.map(items, function(item) {
-									return _.mapObject(feed.itemAttributePaths, function(path, key) {
-										var value = getValueAtPath(item, path);
+				var requestOptions = _.result(feed, 'requestOptions');
+				async.seq(
+					function(next) {
+						options.request(requestOptions, function(error, response) {
+							if (error) return next(error);
+							next(null, response);
+						});
+					},
+					function(response, next) {
+						if (response.statusCode >= 400) {
+							return next(new Error(response.statusMessage));
+						}
+						parseXml(response.body, next);
+					},
+					function(xml, next) {
+						try {
+							var groups = getValueAtPath(xml, feed.paths.group);
+							_.each(groups, function(group) {
+								var items = getValueAtPath(group, feed.paths.item);
+								items = _.chain(items).map(function(item) {
+									return _.chain(feed.paths.attributes).mapObject(function(path, key) {
+										return getValueAtPath(item, path);
+									}).mapObject(function(value, key, context) {
 										var parse = feed.parseAttributes[key];
 										if (parse) {
 											if (_.isString(parse)) {
@@ -78,22 +76,24 @@ module.exports = {
 													value = match && match[1] || null;
 												}
 											} else if (_.isFunction(parse)) {
-												value = parse(value);
+												value = parse.call(context, value);
 											}
 										}
 										return value;
-									});
-								});
+									}).value();
+								}).filter(function(item) {
+									return !_.isEmpty(item);
+								}).value();
 								onData(items);
-							} catch (error) {
-								return next(error);
-							}
+							});
+						} catch (error) {
+							return next(error);
 						}
-					)(function(error) {
-						if (error) onError(error);
-						nextUrl();
-					});
-				}, nextFeed);
+					}
+				)(function(error) {
+					if (error) onError(error);
+					nextFeed();
+				});
 			}, function(error) {
 				if (error) onError(error);
 				onEnd();
